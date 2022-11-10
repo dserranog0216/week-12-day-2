0449896e30e83d5c4c20d7c5796f0fd7ee1f3330
David Serrano <113551943+dserranog0216@users.noreply.github.com>
2022-11-10 20:13:06 +0000

    args thing

--- main.py
+++ main.py
@@ -11,4 +11,20 @@ print(my_sum(3,4,5))
 # print(a_sum(1,3))
 # print(a_sum(1,3,4,5,6))      
 
-def sum_numbers(num1,num2,num3)
+list = [3,4,5,6,7,8,9,50,70,90]
+def sum_numbers(list):
+  # sum = 0
+  # for num in list:
+  #   sum += num
+  # return sum
+  return sum(list)
+print(sum_numbers(list))
+
+#args
+def a_sum(*args):
+  # total = 0
+  # for num in args:
+  #   total += num
+  # return total
+  return sum(args)
+print(a_sum(4,56,70,99,20,9))
