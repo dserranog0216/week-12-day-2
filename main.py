c122d6ffeab2ce1597695ef556e5ec67720c2f07
David Serrano <113551943+dserranog0216@users.noreply.github.com>
2022-11-10 20:04:21 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+def my_sum(*args):
+  print(*args)
+
+print(my_sum(3,4,5))
+#args are for entering as many arguments as possible
+# def a_sum(*args):
+#   for num in args:
+#     print(num)
+
+# print(a_sum(1)
+# print(a_sum(1,3))
+# print(a_sum(1,3,4,5,6))      
+
+def sum_numbers(num1,num2,num3)
